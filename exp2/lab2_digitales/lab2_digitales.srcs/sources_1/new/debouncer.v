a6836bc31c011272b3b9ce98c880fd09be9348a1
jose larrain <jnlarrain@uc.cl>
2018-08-30 17:38:39 -0300

    inicio

---
+++ exp2/lab2_digitales/lab2_digitales.srcs/sources_1/new/debouncer.v
@@ -0,0 +1,72 @@
+module debouncer(
+);
+endmodule
+
+
+
+
+//library IEEE;
+//use IEEE.STD_LOGIC_1164.ALL;
+//use IEEE.std_logic_unsigned.all;
+//USE IEEE.NUMERIC_STD.ALL;
+//use IEEE.math_real.all;
+
+//entity Debouncer is
+//    Generic ( DEBNC_CLOCKS : INTEGER range 2 to (INTEGER'high) := 2**16;
+//          PORT_WIDTH : INTEGER range 1 to (INTEGER'high) := 5);
+//    Port ( clk : in  std_logic;
+//           datain : in  std_logic_vector ((PORT_WIDTH - 1) downto 0);
+//           dataout : out  std_logic_vector ((PORT_WIDTH - 1) downto 0));
+//end Debouncer;
+
+//architecture Behavioral of Debouncer is
+//constant CNTR_WIDTH : integer := natural(ceil(LOG2(real(DEBNC_CLOCKS))));
+//constant CNTR_MAX : std_logic_vector((CNTR_WIDTH - 1) downto 0) := std_logic_vector(to_unsigned((DEBNC_CLOCKS - 1), CNTR_WIDTH));
+//type VECTOR_ARRAY_TYPE is array (integer range <>) of std_logic_vector((CNTR_WIDTH - 1) downto 0);
+
+//signal sig_cntrs_ary : VECTOR_ARRAY_TYPE (0 to (PORT_WIDTH - 1)) := (others=>(others=>'0'));
+
+//signal sig_out_reg : std_logic_vector((PORT_WIDTH - 1) downto 0) := (others => '0');
+
+//begin
+
+//debounce_process : process (clk)
+//begin
+//   if (rising_edge(clk)) then
+//   for index in 0 to (PORT_WIDTH - 1) loop
+//      if (sig_cntrs_ary(index) = CNTR_MAX) then
+//         sig_out_reg(index) <= not(sig_out_reg(index));
+//      else
+//            -- necesario para poder simular
+//      end if;
+//   end loop;
+//   else
+//        -- necesario para poder simular
+//   end if;
+//end process;
+
+//counter_process : process (clk)
+//begin
+//	if (rising_edge(clk)) then
+//	for index in 0 to (PORT_WIDTH - 1) loop
+	
+//		if ((sig_out_reg(index) = '1') xor (datain(index) = '1')) then
+//			if (sig_cntrs_ary(index) = CNTR_MAX) then
+//				sig_cntrs_ary(index) <= (others => '0');
+//			else
+//				sig_cntrs_ary(index) <= sig_cntrs_ary(index) + 1;
+//			end if;
+//		else
+//			sig_cntrs_ary(index) <= (others => '0');
+//		end if;
+		
+//	end loop;
+	
+//	else
+//	       -- necesario para poder simular
+//	end if;
+//end process;
+
+//dataout <= sig_out_reg;
+
+//end Behavioral;
---
+++ exp2/lab2_digitales/lab2_digitales.srcs/sources_1/new/siete_seg.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/30/2018 03:25:44 PM
+// Design Name: 
+// Module Name: siete_seg
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module siete_seg(
+
+    );
+endmodule
