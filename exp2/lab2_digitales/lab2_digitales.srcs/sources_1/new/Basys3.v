e459c13b4ba3a19f4c7c47f2e017e939bfdc1388
jose larrain <jnlarrain@uc.cl>
2018-09-06 00:27:32 -0300

    avance200

--- exp2/lab2_digitales/lab2_digitales.srcs/sources_1/new/Basys3.v
+++ exp2/lab2_digitales/lab2_digitales.srcs/sources_1/new/Basys3.v
@@ -9,25 +9,41 @@ module Basys3(
     output an
     );
     
-    // 
+    // Falta realizar los wires internos para las conecciones de los diferentes modulos
+    // Falta realizar las operaciones propias de los wires
+    // Falta seleccionar las frecuencias y senal
+    // Falta selector de boton
+    // Falta encender leds
     assign an = 'b00;
     
+    // Listo llegar y conectar (creo)
     display(    
     .display(Fsel),
     .clk(clk),
     .seg(seg)
     );  
     
+    // Falta por hacer
     mem();
     
-    clock_divider();
+    // Falta por conectar
+    clock_divider(
+    .clk(clk),
+    .speed(Sspeed),
+    .clock(clock)
     
+    );
+    
+    //Falta por conectar
     mux();
     
+    //Falta por hacer
     cont_1();
     
+    //Falta por hacer
     cont_2();
     
+    //Falta por hacer
     debouncer();
     
 endmodule
--- exp2/lab2_digitales/lab2_digitales.srcs/sources_1/new/clock_divider.v
+++ exp2/lab2_digitales/lab2_digitales.srcs/sources_1/new/clock_divider.v
@@ -1,48 +1,36 @@
 `timescale 1ns / 1ps
 
 module clock_divider( 
-    input clk, speed,
+    input clk,
+    input [1:0] speed,
     output clock
     );
+
+
+reg [26:0] clock_divide_max;
+reg [26:0] clock_divide_counter;
+reg internal_clock;
+
+// diferentes velocidad del clock como constantes
+wire [26:0] full_max    = 'b000000000000000000000000100;
+wire [26:0] fast_max    = 'b000010111110101111000010000;
+wire [26:0] normal_max  = 'b001011111010111100001000000;
+wire [26:0] slow_max    = 'b101111101011110000100000000;
+
+
+
+always @(*)
+begin
+clock_divide_max = speed[1] ? (speed[0] ? full_max : fast_max) : (speed[0] ? normal_max : slow_max);
+end
+
+
+always @(clk)
+if(clock_divide_counter > clock_divide_max) clock_divide_counter = 'b0;
+else if (clock_divide_counter == clock_divide_max) internal_clock = ~internal_clock;
+else clock_divide_counter = clock_divide_counter + 1;
+
+
+assign clock = internal_clock;
 endmodule
 
-// Para seleccionar velocidades
-
-
-//architecture Behavioral of Clock_Divider is
-
-//constant full_max         : std_logic_vector(26 downto 0) := "000000000000000000000000001";
-//constant fast_max         : std_logic_vector(26 downto 0) := "000010111110101111000010000";
-//constant normal_max       : std_logic_vector(26 downto 0) := "001011111010111100001000000";
-//constant slow_max         : std_logic_vector(26 downto 0) := "101111101011110000100000000";
-
-//signal clock_divide_max     : std_logic_vector(26 downto 0) := (others => '0');
-//signal clock_divide_counter : std_logic_vector(26 downto 0) := (others => '0');
-//signal internal_clock       : std_logic := '0';
-    
-//begin
-
-//with speed select
-//    clock_divide_max <= full_max    when "00",
-//                        fast_max    when "01",
-//                        normal_max  when "10",
-//                        slow_max    when "11",
-//                        normal_max  when others;
-                        
-//clock_divide : process (clk)
-//    begin
-//        if (rising_edge(clk)) then
-//            if (clock_divide_counter >= clock_divide_max) then
-//                clock_divide_counter <= (others => '0');
-//                internal_clock <= not internal_clock;
-//            else
-//                clock_divide_counter <= clock_divide_counter + 1;
-//            end if;
-//        else
-//                -- necesario para poder simular
-//        end if;
-//    end process;
-    
-//clock <= internal_clock;
-
-//end Behavioral;
