7be4766636bbc49705a4260762d8ba9ae482c5a7
jose larrain <jnlarrain@uc.cl>
2018-10-09 17:58:52 -0300

    nuevos componentes

---
+++ exp4/lab4.srcs/sources_1/new/door_led_mgmt.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 10/09/2018 05:58:04 PM
+// Design Name: 
+// Module Name: door_led_mgmt
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module door_led_mgmt(
+
+    );
+endmodule
