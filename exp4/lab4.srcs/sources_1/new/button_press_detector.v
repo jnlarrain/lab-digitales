490deac07efc78beeb69be4404cb6652d7a5af9e
Alejandro <akaminetzkp@gmail.com>
2018-10-10 00:44:33 -0300

    Add button_press_detector.v

--- exp4/lab4.srcs/sources_1/new/button_press_detector.v
+++ exp4/lab4.srcs/sources_1/new/button_press_detector.v
@@ -1,5 +1,13 @@
 module button_press_detector(
-
+    input clk,
+    input [15:0] btns,
+    output [5:0] floor_press,
+    output [3:0] el_press_1,
+    output [3:0] el_press_2
     );
+    
+    assign floor_press[5:0] = ~{btns[5], btns[8], btns[9], btns[12], btns[13], btns[0]};
+    assign el_press_1[3:0] = ~{btns[6], btns[10], btns[14], btns[2]};
+    assign el_press_2[3:0] = ~{btns[7], btns[11], btns[15], btns[3]};
     
 endmodule
