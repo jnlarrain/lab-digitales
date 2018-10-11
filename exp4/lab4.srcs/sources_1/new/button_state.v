f0d142038aab16e4915ac4a3701e2090f5fe73eb
Alejandro <akaminetzkp@gmail.com>
2018-10-11 13:16:56 -0300

    Modify button_state.v

--- exp4/lab4.srcs/sources_1/new/button_state.v
+++ exp4/lab4.srcs/sources_1/new/button_state.v
@@ -11,19 +11,36 @@ module button_state(
     output reg [3:0] el_state_2
     );
     
+    initial
+    begin
+        floor_state = 6'b000000;
+        el_state_1 = 4'b0000;
+        el_state_2 = 4'b0000;
+    end
+    
+    reg [28:0] counter;
+    parameter MAXVAL = 100000000;
+    
     always @ (posedge clk)
     begin
-//        if (floor_press[0])
-//            floor_state[0] = 1;
-//        else if (floor_press[1])
-//            floor_state[0] = 0;
-        floor_state = floor_state | floor_press;
-        el_state_1 = el_state_1 | el_press_1;
-        el_state_2 = el_state_2 | el_press_2;
-        
-         floor_state = floor_state & ~floor_pull;
-         el_state_1 = el_state_1 & ~el_pull_1;
-         el_state_2 = el_state_2 & ~el_pull_2;
+        if (counter < MAXVAL)
+        begin
+            counter = counter + 1;
+        end
+        else
+        begin
+    //        if (floor_press[0])
+    //            floor_state[0] = 1;
+    //        else if (floor_press[1])
+    //            floor_state[0] = 0;
+            floor_state = floor_state | floor_press;
+            el_state_1 = el_state_1 | el_press_1;
+            el_state_2 = el_state_2 | el_press_2;
+            
+            floor_state = floor_state & ~floor_pull;
+            el_state_1 = el_state_1 & ~el_pull_1;
+            el_state_2 = el_state_2 & ~el_pull_2;
+        end
     end
     
     
