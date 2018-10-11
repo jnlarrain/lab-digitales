68ab8cff050d5ecba705f1b0349db9fcf42fbd84
Alejandro <akaminetzkp@gmail.com>
2018-10-11 02:00:23 -0300

    Add button_puller.v and modify main_ale.v

--- exp4/lab4.srcs/sources_1/new/main_ale.v
+++ exp4/lab4.srcs/sources_1/new/main_ale.v
@@ -21,6 +21,10 @@ module main_ale(
     wire [2:0] set_1;
     wire [2:0] set_2;
     
+    wire [5:0] floor_pull;
+    wire [3:0] el_pull_1;
+    wire [3:0] el_pull_2;
+    
     move_elevator #(2)(clk, pulse, set_1, pos_1, static_1, dir_1);
     move_elevator #(6)(clk, pulse, set_2, pos_2, static_2, dir_2);
     
@@ -29,9 +33,11 @@ module main_ale(
     
     assign led[0] = static_1;
     assign led[1] = static_2;
-    assign led[2] = dir_1;
-    assign led[3] = dir_2;
+    assign led[15:10] = floor_pull;
+    assign led[9:6] = el_pull_2;
+    assign led[5:2] = el_pull_1;
     
     seven_seg_mgmt_test(clk, 1, pos_2, pos_1, an, seg);
+    button_puller(clk, pos_1, pos_2, static_1, static_2, floor_pull, el_pull_1, el_pull_2);
     
 endmodule
