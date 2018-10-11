90a10fefec69de873cba3ecd5697f7f699b7ab69
Alejandro <akaminetzkp@gmail.com>
2018-10-11 03:34:14 -0300

    Modify main_ale.v and add comments

--- exp4/lab4.srcs/sources_1/new/main_ale.v
+++ exp4/lab4.srcs/sources_1/new/main_ale.v
@@ -2,42 +2,79 @@
 
 module main_ale(
     input clk,
-    input [15:0] sw,
+    input [3:0] JC,
+    output [3:0] JB,
     output [15:0] led,
     output [6:0] seg,
     output [3:0] an
     );
     
-    wire pulse;
-    pulse_generator #(150000000)(clk, pulse);
+    // Este pulso se usa en door_led_mgmt y en logic_mgmt.
+    wire pulse_slow;
+    // Este pulso se usa para el movimiento del ascensor. Su periodo es 1.5 segundos.
+    wire pulse_fast;
+    // Este es un pulso de alta frecuencia usado para el keypad.
+    wire pulse_keypad;
     
+    // Esta es una variable intermedia del keypad.
+    wire [15:0] btns;
+    
+    // Posiciones de los ascensores. Van entre 0 y 6.
     wire [2:0] pos_1;
     wire [2:0] pos_2;
+    // Indican si el ascensor esta estatico o no.
     wire static_1;
     wire static_2;
+    // Indican la direccion del ascensor. 1 si el ascensor sube.
     wire dir_1;
     wire dir_2;
     
+    // Cada variable contiene el piso deseado. Van entre 0 y 6.
     wire [2:0] set_1;
     wire [2:0] set_2;
     
+    // Estas variables almacenan los pedidos actuales.
+    wire [5:0] floor_state;
+    wire [3:0] el_state_1;
+    wire [3:0] el_state_2;
+    
+    // Estas variables tienen el estado real de los botones.
+    wire [5:0] floor_press;
+    wire [3:0] el_press_1;
+    wire [3:0] el_press_2;
+    
+    // Estas variables se usan para modificar los state cuando se satisface un pedido.
     wire [5:0] floor_pull;
     wire [3:0] el_pull_1;
     wire [3:0] el_pull_2;
     
-    move_elevator #(2)(clk, pulse, set_1, pos_1, static_1, dir_1);
-    move_elevator #(6)(clk, pulse, set_2, pos_2, static_2, dir_2);
+    // 1 si la puerta esta abierta.
+    wire door_led;
+    // Se emite un pulso cuando la puerta se cierra.
+    wire close_pulse;
+    // FALTA IMPLEMENTAR QUE SE ABRA LA PUERTA
     
-    assign set_1 = sw[2:0] > 6 ? 6 : sw[2:0];
-    assign set_2 = sw[5:3] > 6 ? 6 : sw[5:3];
+    // Generadores de pulsos.
+    pulse_generator (clk, pulse_slow);
+    pulse_generator #(150000000)(clk, pulse_fast);
+    pulse_generator_keypad(clk, pulse_keypad);
     
-    assign led[0] = static_1;
-    assign led[1] = static_2;
-    assign led[15:10] = floor_pull;
-    assign led[9:6] = el_pull_2;
-    assign led[5:2] = el_pull_1;
-    
-    seven_seg_mgmt_test(clk, 1, pos_2, pos_1, an, seg);
+    // Modulos relacionados al keypad y el estado de los pedidos.
+    keypad(clk, pulse_keypad, JC[3:0], JB[3:0], btns[15:0]);
+    button_press_detector(clk, btns, floor_press, el_press_1, el_press_2);
+    button_state(clk, floor_press, el_press_1, el_press_2, floor_pull, el_pull_1, el_pull_2, floor_state, el_state_1, el_state_2);
     button_puller(clk, pos_1, pos_2, static_1, static_2, floor_pull, el_pull_1, el_pull_2);
+    
+    // Modulos para mover ascensores
+    move_elevator #(2)(clk, pulse_fast, set_1, pos_1, static_1, dir_1);
+    move_elevator #(6)(clk, pulse_fast, set_2, pos_2, static_2, dir_2);
+    
+    // Este close_pulse le avisa al logic_mgmt que ya se cerro la puerta. Es un pulso.
+    door_led_mgmt(clk, 0, door_led, close_pulse);
+    
+    // Leds para mostrar info.
+    assign led[0] = dir_1;
+    assign led[1] = dir_2;
+    assign led[2] = door_led;
     
 endmodule
